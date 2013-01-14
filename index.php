2a457c669bde19779d8f2523d2fcf88faaeb5110
Darrell De Freitas <darrell.defreitas@gmail.com>
2013-01-13 23:36:32 -0500

    Downtime Message

--- index.php
+++ index.php
@@ -8,19 +8,33 @@ include('classes/Common.php');
 printHeader("darrelld - Index!"); 
 ?>
 <body>
-    <span id ="banner">Hello Friend! What you see before you is a work in progress...things might break or just look funny(including the color scheme).</span>
 
-	<?php printNav($_SERVER['PHP_SELF']);?>
+	<?php //printNav($_SERVER['PHP_SELF']);?>
 	
 	<?php
-		//Display the latest post
+		/*//Display the latest post
 		$query = "SELECT * from posts ORDER BY id DESC LIMIT 1";
 		$result = $mySQL_connection->query($query);
 		$post = $result->fetch_assoc();
 		$rowCount =  $result->num_rows;
 		
-		
-		printPosts($post);
+		$title = $post['title'];
+		$postBody = $post['post'];
+		$author = $post['poster'];
+		$date = $post['date'];
+		$tags = $post['tags'];
+		printPosts($post);*/
+                printPosts(
+                        array(
+                            "id" => "999",
+                            "post" => "Ah downtime.
+                                
+I'm having some issues with my hosting company. Lots of data loss and apologies all around from them. I'll be re-configuring shortly and getting everything back up and running once I get a chance to sit down and properly reconfigure everything.",
+                            "title" => "Be Right Back",
+                            "poster" => "darrelld",
+                            "date" => "1358137634",
+                            "tags" => "maintenance"
+                            ))
 		
 	?>
 	
