71c412634ca6ba849ab61e265bf60080a90231cb
Darrell De Freitas <darrell.defreitas@gmail.com>
2013-03-05 21:58:38 -0500

    Fixed comment bug

--- sql/saveComments.php
+++ sql/saveComments.php
@@ -1,8 +1,9 @@
 <?php
     include_once(dirname(__FILE__) .' /../classes/Comments.php');
+    include_once(dirname(__FILE__) .' /../classes/Common.php');
     
     $name = clean($_POST['name']);
-    $comment = clean(['comment']);
+    $comment = clean($_POST['comment']);
     $id = clean($_POST['id']);
 
     Comments::saveComment($id,$name,$comment, time());
