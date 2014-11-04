a57a47ae629785208ca548e41f55b0ad0c0cc630
Darrell De Freitas <darrell.defreitas@gmail.com>
2014-11-04 00:21:53 -0500

    CSS fix, default date setting added

--- classes/Common.php
+++ classes/Common.php
@@ -5,6 +5,7 @@ include_once('markdown.php');
 
 global $mySQL_connection;
 $settings = parse_ini_file(dirname(__FILE__) . '/../config.ini');
+date_default_timezone_set("America/New_York");
 
 //$mySQL_connection = new MySQL($mysql_server,$mysql_user,$mysql_pass,$mysql_db);
 $_SESSION['mySQL_connection'] = $mySQL_connection;
