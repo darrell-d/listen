e36a5dd318dab331d17ef605a69c94866b2fe089
Darrell De Freitas <darrell.defreitas@gmail.com>
2014-11-05 16:31:39 -0500

    Text analyzer added

--- classes/MarkdownReader.php
+++ classes/MarkdownReader.php
@@ -1,4 +1,17 @@
 <?php
+
+/**
+MARKDOWN -> TEXT | SPECIAL_CHAR;
+TEXT -> TEXT | SPECIAL_CHAR;
+SPECIAL_CHAR-> SPECIAL_CHAR | TEXT;
+
+SPECIAL_CHAR: #,`,*,[,>;
+#: #{1+};
+`:`TEXT{1+}`;
+\*:*{1-2}TEXT{1+}*{1-2}
+[: [TEXT](TEXT);
+>:>TEXT{1+}
+**/
 include('Symbols.php');
 class MarkdownReader
 {
@@ -17,7 +30,7 @@ class MarkdownReader
       while(!feof($file))
       {
         $line = fgets($file);
-        $this->output .= $this->parse($line);
+        $this->output .= $this->analyze($line);
       }
       fclose($file);
     }
@@ -28,6 +41,64 @@ class MarkdownReader
     //echo $this->output ;
   }
 
+  function analyze($line)
+  {
+    if(strpos($line,"#") === 0)
+    {
+      echo Symbols::HASH . PHP_EOL . "<br>";
+    }
+    else if(strpos($line,"`") === 0)
+    {
+      echo Symbols::BACK_TICK . PHP_EOL. "<br>";
+    }
+    else if(strpos($line,"*") === 0)
+    {
+      echo Symbols::ASTERIX . PHP_EOL. "<br>";
+    }
+    else if(strpos($line,"[") === 0)
+    {
+      echo Symbols::SQUARE_BRACE_L . PHP_EOL. "<br>";
+    }
+    else if(strpos($line,">") === 0)
+    {
+      echo Symbols::POINTY_BRACE_R . PHP_EOL. "<br>";
+    }
+    else
+    {
+      echo Symbols::TEXT;
+      $this->analyzeText($line);
+    }
+  }
+
+  function analyzeText($text)
+  {
+    //Split up characters
+    if(!empty($text))
+    {
+      $chars = str_split($text);
+      echo ": ";
+      foreach($chars as $key => $value)
+      {
+        if($value == str_split(Symbols::BACK_TICK)[0])
+        {
+          echo Symbols::BACK_TICK;
+        }
+        else if($value == str_split(Symbols::ASTERIX)[0])
+        {
+          echo Symbols::ASTERIX;
+        }
+        else if($value == str_split(Symbols::SQUARE_BRACE_L)[0])
+        {
+          echo Symbols::SQUARE_BRACE_L;
+        }
+        else if($value == str_split(Symbols::POINTY_BRACE_R)[0])
+        {
+          echo Symbols::POINTY_BRACE_R;
+        }
+      }
+      echo "<br>";
+    }
+  }
   function parse($line)
   {
     $this->line_count++;
--- classes/Symbols.php
+++ classes/Symbols.php
@@ -2,9 +2,17 @@
 
 class Symbols
 {
-  const RAW_TEXT = 0;
-  const CHARACTER = 1;
-  const SPECIAL_CHAR = 2;
+  const HASH = "#_SYM_HASH";
+  const BACK_TICK = "`_SYM_B_TICK";
+  const ASTERIX  = "*_SYM_ASTX";
+  const SQUARE_BRACE_L = "[_SYM_SQ_BR_L";
+  const SQUARE_BRACE_R = "]_SYM_SQ_BR_R";
+  const BRACE_L = "(_SYM_BR_L";
+  const BRACE_R = ")_SYM_BR_R";
+  const POINTY_BRACE_R = ">_SYM_PTY_BR_R";
+  const TEXT = "SYM_TEXT";
+  const MARKDOWN = "SYM_MARKDOWN";
+  const SPECIAL_CHAR = "SYM_SP_CHAR";
 
 }
 
